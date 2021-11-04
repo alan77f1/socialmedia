354c560681dbcbfd5a4af14a466ece9a04ce72d7
buivantan01 <buivantan1234566@gmail.com>
2021-11-04 10:00:25 +0700

    Create Home page

--- api/routers/messageRoute.js
+++ api/routers/messageRoute.js
@@ -4,23 +4,25 @@ const router = express.Router();
 
 // create a message [POST] api/messages/
 router.post('/', async (req, res) => {
-    try {
-        const message = new Message(req.body);
-        const savedMessage = await message.save();
-        res.status(200).json(savedMessage);
-    } catch (error) {
-        res.status(500).json(error);
-    }
+  try {
+    const message = new Message(req.body);
+    const savedMessage = await message.save();
+    res.status(200).json(savedMessage);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // get message by conversationId [GET] api/messages/
 router.get('/:conversationId', async (req, res) => {
-    try {
-        const messages = await Message.find({ conversationId: req.params.conversationId });
-        res.status(200).json(messages);
-    } catch (error) {
-        res.status(500).json(error);
-    }
+  try {
+    const messages = await Message.find({
+      conversationId: req.params.conversationId,
+    });
+    res.status(200).json(messages);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
