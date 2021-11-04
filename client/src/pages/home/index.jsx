42bbe69c259245e195643c30939589d7ce51990e
buivantan01 <buivantan1234566@gmail.com>
2021-11-04 10:56:56 +0700

    Add some for home page

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import './Home.css';
 
-const Home = () => {
-  return <div>Home</div>;
-};
+Home.propTypes = {};
+
+function Home(props) {
+  return (
+    <div>
+      <div className='container'>
+        <div className='homePage'>
+          <div className='cLeft'>SideBar</div>
+          <div className='cMiddle'>Feed</div>
+          <div className='cRight'>RightBar</div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Home;
