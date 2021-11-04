125c623127f3543b3abcae0a899f7fb063fc3efd
buivantan01 <buivantan1234566@gmail.com>
2021-11-04 13:55:03 +0700

    Create section register

--- client/src/pages/login/index.jsx
+++ client/src/pages/login/index.jsx
@@ -26,8 +26,141 @@ function Login(props) {
           </form>
         </div>
       </div>
-    </div>
-  );
+       {/* Register */}
+       {openRegister && (
+                <div className="registerModalWrap">
+                    <div className="registerModal">
+                        <div className="registerTop">
+                            <div className="registerTopText">
+                                <div className="registerTopTextTitle">Đăng ký</div>
+                                <div className="registerTopTextDesc">Nhanh chóng và dễ dàng.</div>
+                            </div>
+                            <div className="registerTopIcon" />
+                            </div>
+                        </div>
+                        <hr className="registerHr" />
+                        <form className="registerForm" >
+                            <div className="grid__row">
+                                <div className=" grid__col-6-12">
+                                    <div className="grid__col">
+                                        <div className="registerInput">
+                                            <input type="text" placeholder="Họ" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className=" grid__col-6-12">
+                                    <div className="grid__col">
+                                        <div className="registerInput">
+                                            <input type="text" placeholder="Tên" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className=" grid__col-12-12">
+                                    <div className="grid__col">
+                                        <div className="registerInput">
+                                            <input
+                                                type="text"
+                                                placeholder="Số di động hoặc email"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className=" grid__col-12-12">
+                                    <div className="grid__col">
+                                        <div className="registerInput">
+                                            <input
+                                                type="text"
+                                                placeholder="Mật khẩu"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="selectWrap">
+                                    <div className="selectWrapDesc">Ngày sinh</div>
+                                    <div className=" grid__col-4-12">
+                                        <div className="grid__col">
+                                            <div className="registerInput">
+                                                <select >
+                                                    {Array.from(new Array(31)).map((x, index) => (
+                                                        <option key={index} value={index + 1}>
+                                                            {index + 1}
+                                                        </option>
+                                                    ))}
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className=" grid__col-4-12">
+                                        <div className="grid__col">
+                                            <div className="registerInput">
+                                                <select >
+                                                    {Array.from(new Array(12)).map((x, index) => (
+                                                        <option
+                                                            key={index}
+                                                            value={index + 1}
+                                                        >{`Tháng ${index + 1}`}</option>
+                                                    ))}
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className=" grid__col-4-12">
+                                        <div className="grid__col">
+                                            <div className="registerInput">
+                                                <select name="cars" id="cars">
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="selectWrap">
+                                    <div className=" grid__col-4-12">
+                                        <div className="grid__col">
+                                            <div className="registerInput">
+                                                <label htmlFor="male">Nữ</label>
+                                                <input
+                                                    type="radio"
+                                                    id="male"
+                                                    name="sex"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className=" grid__col-4-12">
+                                        <div className="grid__col">
+                                            <div className="registerInput">
+                                                <label htmlFor="female">Nam</label>
+                                                <input
+                                                    type="radio"
+                                                    id="female"
+                                                    name="sex"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className=" grid__col-4-12">
+                                        <div className="grid__col">
+                                            <div className="registerInput">
+                                                <label htmlFor="orther">Khác</label>
+                                                <input
+                                                    type="radio"
+                                                    id="orther"
+                                                    name="sex"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="submit" className="registerBtn">
+                                Đăng ký
+                            </button>
+                        </form>
+             
+            );
+            </div> 
+    );
 }
 
 export default Login;
