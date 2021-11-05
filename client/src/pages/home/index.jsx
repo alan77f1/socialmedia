675a8e45086745f0c55cdb9364b88a7eb0cef778
buivantan01 <buivantan1234566@gmail.com>
2021-11-05 09:57:07 +0700

    Create contants

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -3,17 +3,17 @@ import './Home.css';
 Home.propTypes = {};
 
 function Home(props) {
-  return (
-    <div>
-      <div className='container'>
-        <div className='homePage'>
-          <div className='cLeft'>SideBar</div>
-          <div className='cMiddle'>Feed</div>
-          <div className='cRight'>RightBar</div>
+    return (
+        <div>
+            <div className='container'>
+                <div className='homePage'>
+                    <div className='cLeft'>SideBar</div>
+                    <div className='cMiddle'>Feed</div>
+                    <div className='cRight'>RightBar</div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Home;
