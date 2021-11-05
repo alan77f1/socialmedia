8078ae42c114bba959e60e7c2eecf4efb00251a9
buivantan01 <buivantan1234566@gmail.com>
2021-11-05 10:36:24 +0700

    Create section post component

---
+++ client/src/components/Post/index.jsx
@@ -0,0 +1,32 @@
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import PublicIcon from '@material-ui/icons/Public';
+import { format } from 'timeago.js';
+
+Post.propTypes = {};
+
+function Post({ post, currentUser }) {
+    return (
+        <div className='post'>
+            <div className='postTop'>
+                <img src='' alt='' className='postTopAvatar' />
+                <div className='postTopInfo'>
+                    <span className='postTopInfoName'>post</span>
+                    <div className='postTopInfoTime'>
+                        <span>{format(post.createdAt)} · </span>{' '}
+                        <PublicIcon style={{ fontSize: 'inherit' }} />
+                    </div>
+                </div>
+                <div className='postTopAction'>
+                    <MoreHorizIcon />
+                </div>
+            </div>
+
+            <div className='postCommentWrap'>
+                <hr className='postHr' />
+                Comment
+            </div>
+        </div>
+    );
+}
+
+export default Post;
