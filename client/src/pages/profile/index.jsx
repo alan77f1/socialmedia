ee70b1cf0bd9521ab6fc55fe4888aacb5967a57c
buivantan01 <buivantan1234566@gmail.com>
2021-11-05 10:07:13 +0700

    Handle profile Main for profile

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,3 +1,5 @@
+import './Profile.css';
+
 Profile.propTypes = {};
 
 function Profile(props) {
@@ -27,6 +29,64 @@ function Profile(props) {
                     </div>
                     <hr className='profileTopHr' />
                 </div>
+            </div>
+            <div className='profileMain'>
+                <div className='profileMainLeft'>
+                    <div className='mainIntroduce'>
+                        <div className='mainIntroduceTitle'>Giới thiệu</div>
+                        <ul className='mainIntroduceList'>
+                            <li className='mainIntroduceItem'>
+                                <img
+                                    src='./assets/profile/bagIcon.png'
+                                    alt=''
+                                    className='mainIntroduceItemBadge'
+                                />
+                                <div className='mainIntroduceItemText'>
+                                    Đã tốt nghiệp tại THPT Sơn Thịnh
+                                </div>
+                            </li>
+                            <li className='mainIntroduceItem'>
+                                <img
+                                    src='./assets/profile/bagIcon.png'
+                                    alt=''
+                                    className='mainIntroduceItemBadge'
+                                />
+                                <div className='mainIntroduceItemText'>
+                                    sinh viên tại Đại học Bách khoa Hà Nội - Hanoi University of
+                                    Science and Technology
+                                </div>
+                            </li>
+                            <li className='mainIntroduceItem'>
+                                <img
+                                    src='./assets/profile/homeIcon.png'
+                                    alt=''
+                                    className='mainIntroduceItemBadge'
+                                />
+                                <div className='mainIntroduceItemText'>Sống tại Yên Bái</div>
+                            </li>
+                            <li className='mainIntroduceItem'>
+                                <img
+                                    src='./assets/profile/heartIcon.png'
+                                    alt=''
+                                    className='mainIntroduceItemBadge'
+                                />
+                                <div className='mainIntroduceItemText'>Hẹn hò</div>
+                            </li>
+                            <li className='mainIntroduceItem'>
+                                <img
+                                    src='./assets/profile/clockIcon.png'
+                                    alt=''
+                                    className='mainIntroduceItemBadge'
+                                />
+                                <div className='mainIntroduceItemText'>
+                                    Tham gia vào Tháng 6 năm 2016
+                                </div>
+                            </li>
+                        </ul>
+                        <div className='mainIntroduceBtn'>Chỉnh sửa chi tiết</div>
+                    </div>
+                </div>
+                <div className='profileMainRight'>profileMainRight</div>
             </div>
         </div>
     );
