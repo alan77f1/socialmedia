9a090e3668d14ee1e4789f2a770f9bd4415a0c4d
buivantan01 <buivantan1234566@gmail.com>
2021-11-07 09:17:18 +0700

    Added some handle react Hooks for Feed component

--- client/src/components/Feed/index.jsx
+++ client/src/components/Feed/index.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Post from '../Post';
 import Share from '../Share';
+import axios from 'axios';
 
 import './Feed.css';
+import { sortDateUtils } from '../../utils/utils';
 
 Feed.propTypes = {};
 
 function Feed({ currentUser }) {
     const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const res = await axios.get(`/posts/${currentUser._id}`);
+
+            setPosts(sortDateUtils(res.data));
+            // res.data.sort((a, b) => {
+            //     return -(new Date(a.updatedAt) - new Date(b.updatedAt));
+            // })
+        })();
+    }, [currentUser._id]);
 
     return (
         <div className='feed'>
