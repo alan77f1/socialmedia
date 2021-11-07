619958a7b4869c5b52c804a4dedf71b97998c148
buivantan01 <buivantan1234566@gmail.com>
2021-11-07 09:12:05 +0700

    Create Feed component

--- client/src/components/Feed/index.jsx
+++ client/src/components/Feed/index.jsx
@@ -1,9 +1,25 @@
+import React, { useState } from 'react';
+import Post from '../Post';
+import Share from '../Share';
+
 import './Feed.css';
 
 Feed.propTypes = {};
 
 function Feed({ currentUser }) {
-    return <div className='feed'>Feed</div>;
+    const [posts, setPosts] = useState([]);
+
+    return (
+        <div className='feed'>
+            <Share currentUser={currentUser} setPosts={setPosts} posts={posts} />
+            <Post currentUser={currentUser} />
+            <Post currentUser={currentUser} />
+            <Post currentUser={currentUser} />
+            {posts.map((post) => (
+                <Post key={post._id} currentUser={currentUser} post={post} />
+            ))}
+        </div>
+    );
 }
 
 export default Feed;
