02cbdb9cfd1a013ef83c08c500dfe994ca4d11cf
buivantan01 <buivantan1234566@gmail.com>
2021-11-09 10:24:18 +0700

    Added some handle react Hooks for story detail

--- client/src/pages/story/StoryDetail.jsx
+++ client/src/pages/story/StoryDetail.jsx
@@ -1,16 +1,113 @@
 import axios from 'axios';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { NO_AVARTAR, PF } from '../../constants';
+import { AuthContext } from '../../context/AuthProvider';
 import { sortDateUtils } from '../../utils/utils';
-
 
 StoryDetail.propTypes = {};
 
 function StoryDetail(props) {
+    const { user: currentUser } = useContext(AuthContext);
+    const [stories, setStories] = useState(); /* total story */
+
+    /* index of storyUser: [story1, story2, story3]*/
+    const [showStoryIndex, setShowStoryIndex] = useState(0);
+    const [followingUsers, setFollowingUsers] = useState([]);
+    const [storyViewer, setStoryViewer] = useState([]); /*story will show inside class right */
+    const [storyAuthor, setStoryAuthor] = useState({});
+    const [allUser, setAllUser] = useState([]);
+    const storyTimeOutRef = useRef(null);
+    const pauseFlagMouse = useRef(false); /*flag of mouse event */
+    const [myStories, setMyStories] = useState();
+
+    // why: to hidden btn next and prev
+    const [firstStory, setFirstStory] = useState();
+    const [lastStory, setLastStory] = useState();
+
+    /*flag of btn pause */
+    // const [pauseState, setPauseState] = useState(false); will re-render if use state in this case
+    const pauseFlagBtn = useRef(false);
 
     const { storyId } = useParams();
 
     // get stories and get user from stories
+
+    useEffect(() => {
+        try {
+            (async () => {
+                // get stories
+
+                const storiesRes = await axios.get(`/stories/${currentUser._id}`);
+
+                setStories(storiesRes.data);
+
+                const storyList = storiesRes.data;
+                setFirstStory(sortDateUtils(storyList[0])[0]);
+                const lastStoryUser = sortDateUtils(storyList[storyList.length - 1]);
+                setLastStory(lastStoryUser[lastStoryUser.length - 1]);
+
+                const tempArray = []; /*following story */
+                let userId;
+
+                sortDateUtils(storiesRes.data).forEach((storyUser) => {
+                    // display story viewer
+                    if (storyUser.some((aStoryUser) => aStoryUser._id === storyId)) {
+                        setStoryViewer(sortDateUtils(storyUser));
+                        userId = storyUser[0].userId;
+                    }
+
+                    // display friendlist
+                    if (storyUser[0].userId !== currentUser._id) {
+                        tempArray.push(storyUser);
+                    } else {
+                        setMyStories(storyUser);
+                    }
+                });
+
+                // get author from story viewer
+                const authorRes = await axios.get(`/users?userId=${userId}`);
+                setStoryAuthor(authorRes.data);
+
+                // get following user from stories
+
+                const followingUserIds = tempArray.map(
+                    (followingStory) => followingStory[0].userId
+                );
+
+                const followingUsersRes = await axios.put('/users/list', {
+                    /*get method ko co body*/ userIds: followingUserIds,
+                });
+                setFollowingUsers(followingUsersRes.data);
+                setAllUser([currentUser, ...followingUsersRes.data]);
+            })();
+        } catch (error) {
+            console.log(error);
+        }
+    }, [currentUser, storyId]);
+
+    // debounce with ref
+    useEffect(() => {
+        // console.log('fla', pauseFlagMouse.current);
+        if (storyViewer.length > 0) {
+            let time = 0;
+            storyTimeOutRef.current = setInterval(() => {
+                // console.log(time);
+                // 5s
+                if (!pauseFlagMouse.current) {
+                    time++;
+                    if (time === 20) {
+                        time = 0;
+                        changeStoryIndexHandler(1);
+                    }
+                }
+            }, 250);
+        }
+
+        return () => {
+            clearInterval(storyTimeOutRef.current);
+        };
+    });
 
     const changeStoryViewerHandler = (user) => {
         // get story by userId field
