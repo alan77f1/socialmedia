eee321515fe00142a2ef8effde7d3715902e6f38
buivantan01 <buivantan1234566@gmail.com>
2021-11-12 08:44:13 +0700

    Added some handle React Hooks for progress timeout

--- client/src/pages/story/components/ProgressTimeOut.jsx
+++ client/src/pages/story/components/ProgressTimeOut.jsx
@@ -1,11 +1,50 @@
+import React, { useEffect, useRef, useState } from 'react';
+
 ProgressTimeOut.propTypes = {};
 
 function ProgressTimeOut({ storyViewer, showStoryIndex, pauseFlagMouse }) {
+  const [value, setValue] = useState();
+  const interval = useRef(null);
+
+  // bugggggggg ????????????!!!!!!!!!!!!!!!!!!!!!!!!
+  // ---->> fixed: clearInterval: each interval have a uni key*
+  useEffect(() => {
+    clearInterval(interval.current);
+    setValue(0);
+    if (storyViewer.length > 0) {
+      interval.current = setInterval(() => {
+        let newValue = 0;
+        if (!pauseFlagMouse.current) {
+          setValue((prevValue) => {
+            newValue = prevValue + 1;
+
+            if (newValue >= 100) {
+              clearInterval(interval.current);
+              newValue = 100;
+            }
+
+            return newValue;
+          });
+        }
+      }, 50);
+    }
+  }, [storyViewer, showStoryIndex, pauseFlagMouse]);
+
   return (
     <ul className='storyProgessList'>
       {Array.from(new Array(storyViewer.length)).map((x, index) => (
         <li key={index} className='storyProgessItem'>
-          <div className='storyProgessItemProgress'></div>
+          <div
+            className='storyProgessItemProgress'
+            style={{
+              width:
+                index === showStoryIndex
+                  ? `${value}%`
+                  : index > showStoryIndex
+                  ? `0%`
+                  : '100%',
+            }}
+          ></div>
         </li>
       ))}
     </ul>
