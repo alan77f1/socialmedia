e9eae714872304f1d59c1593930c7bf393c04b4d
buivantan01 <buivantan1234566@gmail.com>
2021-11-13 10:00:04 +0700

    Update app

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import { useContext } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { AuthContext } from './context/AuthProvider';
 import './App.css';
 import Header from './components/Header';
+import { AuthContext } from './context/AuthProvider';
 import Home from './pages/home';
 import Login from './pages/login';
 import Profile from './pages/profile';
+import Story from './pages/story';
 
 function App() {
   const { user: currentUser } = useContext(AuthContext);
+
   return (
     <Router>
       {currentUser && <Header />}
       <Switch>
         <Route path='/profile'>{currentUser ? <Profile /> : <Login />}</Route>
+        <Route path='/stories'>{currentUser ? <Story /> : <Login />}</Route>
         <Route path='/' exact>
           {currentUser ? <Home /> : <Login />}
         </Route>
