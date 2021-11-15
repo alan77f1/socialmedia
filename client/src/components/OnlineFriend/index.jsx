8041571cfc81105dfe59b4346a23e44761b1a4cc
buivantan01 <buivantan1234566@gmail.com>
2021-11-15 07:48:10 +0700

    Create section Online Friend component

---
+++ client/src/components/OnlineFriend/index.jsx
@@ -0,0 +1,49 @@
+import './OnlineFriend.css';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import SearchIcon from '@mui/icons-material/Search';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { NO_AVARTAR, PF } from '../../constants';
+
+OnlineFriend.propTypes = {};
+
+function OnlineFriend({ friends }) {
+  return (
+    <div className='onlineFriend'>
+      <div className='onlineFriendTop'>
+        <div className='onlineFriendTopTitle'>Người liên hệ</div>
+        <div className='onlineFriendTopAction'>
+          <div className='onlineFriendTopActionItem'>
+            <VideoCallIcon />
+          </div>
+          <div className='onlineFriendTopActionItem'>
+            <SearchIcon />
+          </div>
+          <div className='onlineFriendTopActionItem'>
+            <MoreHorizIcon />
+          </div>
+        </div>
+      </div>
+      <ul className='onlineFriendList'>
+        {friends.map((friend) => (
+          <li key={friend._id} className='onlineFirendItem'>
+            <div className='onlineFriendItemAvatarWrap'>
+              <img
+                src={`${PF}/${
+                  friend.avatar ? `person/${friend.avatar}` : NO_AVARTAR
+                }`}
+                alt=''
+                className='onlineFriendItemAvatar'
+              />
+              <div className='onlineFriendItemBadge'></div>
+            </div>
+            <span className='onlineFriendItemName'>
+              {friend.firstName} {friend.lastName}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default OnlineFriend;
