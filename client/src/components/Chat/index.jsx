6f5d318d352e2e86988fd70b404e41f0b10a53e0
buivantan01 <buivantan1234566@gmail.com>
2021-11-24 22:44:34 +0700

    Fixed: section Chat component

--- client/src/components/Chat/index.jsx
+++ client/src/components/Chat/index.jsx
@@ -10,81 +10,99 @@ import { io } from 'socket.io-client';
 import { AuthContext } from '../../context/AuthProvider';
 
 Chat.propTypes = {
-  zoomOutState: PropTypes.func,
+    zoomOutState: PropTypes.func,
 };
 
 function Chat(props) {
-  const { user: currentUser } = useContext(AuthContext);
+    const { user: currentUser } = useContext(AuthContext);
 
-  const zoomOutConversations = conversations.filter(
-    (cov) => cov.isZoomOut === true
-  );
-  const [currentConverastionKey, setCurrentConversationKey] = useState();
+    const { data: conversationsStore, dispatch } = useContext(ConversationsContext);
+    const [conversations, setConversations] = useState([]);
+    const zoomInConversations = conversations.filter((cov) => cov.isZoomOut === false);
+    const zoomOutConversations = conversations.filter((cov) => cov.isZoomOut === true);
+    const [currentConverastionKey, setCurrentConversationKey] = useState();
 
-  const [arrivalMessage, setArrivalMessage] = useState(null);
+    const [arrivalMessage, setArrivalMessage] = useState(null);
 
-  // socket
-  const socket = useRef();
-  useEffect(() => {
-    socket.current = io('ws://localhost:8900');
-    // console.log('render: ', socket.current);
-    socket.current.on('getMessage', (data) => {
-      // console.log('data', data);
-      setArrivalMessage({
-        senderId: data.senderId,
-        text: data.text,
-        createdAt: Date.now(),
-      });
-    });
-  }, []);
+    // socket
+    const socket = useRef();
+    useEffect(() => {
+        socket.current = io('ws://localhost:8900');
+        // console.log('render: ', socket.current);
+        socket.current.on('getMessage', (data) => {
+            // console.log('data', data);
+            setArrivalMessage({
+                senderId: data.senderId,
+                text: data.text,
+                createdAt: Date.now(),
+            });
+        });
+    }, []);
 
-  useEffect(() => {
-    socket.current.emit('addUser', currentUser._id);
-    // socket.current.on('getUsers', (users) => {
-    //     console.log('socket users:', users);
-    // });
-  }, [currentUser]);
+    useEffect(() => {
+        socket.current.emit('addUser', currentUser._id);
+        // socket.current.on('getUsers', (users) => {
+        //     console.log('socket users:', users);
+        // });
+    }, [currentUser]);
 
-  const emitSendMessageHandler = (event, sendMessage) => {
-    socket.current.emit('sendMessage', sendMessage);
-  };
+    useEffect(() => {
+        setConversations(conversationsStore);
+    }, [conversationsStore]);
 
-  return (
-    <>
-      {zoomInConversations.map((conversation, index) => (
-        <ConversationItem
-          setCurrentConversationKey={setCurrentConversationKey}
-          currentConverastionKey={currentConverastionKey}
-          key={index}
-          conversation={conversation}
-          index={index}
-          arrivalMessage={arrivalMessage}
-          emitSendMessage={emitSendMessageHandler}
-        />
-      ))}
-      {zoomOutConversations.map((conversation, index) => (
-        <div
-          key={conversation.key}
-          className='chatZoomOut'
-          style={{ bottom: `${index * (40 + 20) + 40}px` }}
-        >
-          <img
-            src={`${PF}/${
-              conversation.receiver.avatar
-                ? `person/${conversation.receiver.avatar}`
-                : NO_AVARTAR
-            }`}
-            alt=''
-            className='chatZoomOutImg'
-          />
-          <div className='chatZoomOutBadge'></div>
-          <div className='chatZoomOutClose'>
-            <CloseIcon style={{ fontSize: 'inherit' }} />
-          </div>
-        </div>
-      ))}
-    </>
-  );
+    const handleZoomOutState = (conversationKey, zoomState) => {
+        const conversationIndex = conversations.findIndex(
+            (conversation) => conversation.key === conversationKey
+        );
+        const updatedConversation = conversations.splice(conversationIndex, 1)[0];
+        updatedConversation.isZoomOut = zoomState;
+
+        const newConversation = [...conversations, updatedConversation];
+        toggleConversation(newConversation, dispatch);
+    };
+
+    const emitSendMessageHandler = (event, sendMessage) => {
+        socket.current.emit('sendMessage', sendMessage);
+    };
+
+    return (
+        <>
+            {zoomInConversations.map((conversation, index) => (
+                <ConversationItem
+                    setCurrentConversationKey={setCurrentConversationKey}
+                    currentConverastionKey={currentConverastionKey}
+                    key={index}
+                    conversation={conversation}
+                    index={index}
+                    changeZoomState={handleZoomOutState}
+                    arrivalMessage={arrivalMessage}
+                    emitSendMessage={emitSendMessageHandler}
+                />
+            ))}
+            {zoomOutConversations.map((conversation, index) => (
+                <div
+                    key={conversation.key}
+                    className="chatZoomOut"
+                    style={{ bottom: `${index * (40 + 20) + 40}px` }}
+                    onClick={() => handleZoomOutState(conversation.key, false)}
+                >
+                    <img
+                        src={`${PF}/${
+                            conversation.receiver.avatar
+                                ? `person/${conversation.receiver.avatar}`
+                                : NO_AVARTAR
+                        }`}
+                        alt=""
+                        className="chatZoomOutImg"
+                    />
+                    <div className="chatZoomOutBadge"></div>
+                    <div className="chatZoomOutClose">
+                        <CloseIcon style={{ fontSize: 'inherit' }} />
+                    </div>
+                </div>
+            ))}
+        </>
+    );
 }
 
 export default Chat;
