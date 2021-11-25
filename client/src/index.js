1ba8882b06e82412106a6e55758c59778668cbc9
buivantan01 <buivantan1234566@gmail.com>
2021-11-25 19:51:14 +0700

    Update something

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthContextProvider } from './context/AuthProvider';
+import { ConversationsContextProvider } from './context/conversations/ConversationsProvider';
+import { MessagesContextProvider } from './context/message/MessagesProvider';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <AuthContextProvider>
-            <App />
-        </AuthContextProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <AuthContextProvider>
+      <ConversationsContextProvider>
+        <MessagesContextProvider>
+          <App />
+        </MessagesContextProvider>
+      </ConversationsContextProvider>
+    </AuthContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
