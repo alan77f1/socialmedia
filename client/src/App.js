2cfb06598f24baa528ae0170fd04ae42f6393a64
buivantan01 <buivantan1234566@gmail.com>
2021-11-28 09:36:17 +0700

    Create section Register pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,36 +3,36 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { io } from 'socket.io-client';
 import './App.css';
 import Chat from './components/Chat';
-import Header from './components/Header';
 import { AuthContext } from './context/AuthProvider';
 import Home from './pages/home';
 import Login from './pages/login';
+import Register from './pages/register';
 import Profile from './pages/profile';
 import Story from './pages/story';
 
 function App() {
-    const { user: currentUser } = useContext(AuthContext);
+  const { user: currentUser } = useContext(AuthContext);
 
-    // Ifsocket
-    const socket = useRef();
-    useEffect(() => {
-        socket.current = io('ws://localhost:8900');
-    }, []);
+  // Ifsocket
+  const socket = useRef();
+  useEffect(() => {
+    socket.current = io('ws://localhost:8900');
+  }, []);
 
-    return (
-        <Router>
-            {currentUser && <Header />}
-            <Switch>
-                <Route path="/profile">{currentUser ? <Profile /> : <Login />}</Route>
-                <Route path="/stories">{currentUser ? <Story /> : <Login />}</Route>
-                <Route path="/" exact>
-                    {currentUser ? <Home /> : <Login />}
-                </Route>
-            </Switch>
+  return (
+    <Router>
+      {currentUser ? <Home /> : <Register />}
+      <Switch>
+        <Route path="/" exact>
+          <Route path="/profile">{currentUser ? <Profile /> : <Login />}</Route>
+          <Route path="/register"></Route>
+          <Route path="/stories">{currentUser ? <Story /> : <Login />}</Route>
+        </Route>
+      </Switch>
 
-            <Chat />
-        </Router>
-    );
+      <Chat />
+    </Router>
+  );
 }
 
 export default App;
--- client/src/pages/login/index.jsx
+++ client/src/pages/login/index.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useRef, useState } from 'react';
 import './Login.css';
-import CloseIcon from '@mui/icons-material/Close';
 import { loginCall, registerCall } from '../../context/useAuth';
 import { AuthContext } from '../../context/AuthProvider';
 
@@ -27,20 +26,8 @@ function Login(props) {
     loginCall({ email: email.current.value, password: password.current.value }, dispatch);
   };
 
-  const handleRegister = (e) => {
-    e.preventDefault();
-    const newUser = {
-      firstName: firstName.current.value,
-      lastName: lastName.current.value,
-      email: email.current.value,
-      password: password.current.value,
-      birthDay: `${dayOfBirth.current.value}/${monthOfBirth.current.value}/${yearOfBirth.current.value}`,
-      sex: sex.current.value,
-    };
     registerCall(newUser, dispatch);
     window.location.reload();
-
-    // setOpenRegister(false);
   };
 
   console.log(user);
@@ -85,125 +72,6 @@ function Login(props) {
           </form>
         </div>
       </div>
-      {/* Register */}
-      {openRegister && (
-        <div className="registerModalWrap">
-          <div className="registerModal">
-            <div className="registerTop">
-              <div className="registerTopText">
-                <div className="registerTopTextTitle">Đăng ký</div>
-                <div className="registerTopTextDesc">Nhanh chóng và dễ dàng.</div>
-              </div>
-              <div className="registerTopIcon" onClick={() => setOpenRegister(false)}>
-                <CloseIcon />
-              </div>
-            </div>
-            <hr className="registerHr" />
-            <form className="registerForm" onSubmit={handleRegister}>
-              <div className="grid__row">
-                <div className=" grid__col-6-12">
-                  <div className="grid__col">
-                    <div className="registerInput">
-                      <input type="text" placeholder="Họ" ref={firstName} />
-                    </div>
-                  </div>
-                </div>
-                <div className=" grid__col-6-12">
-                  <div className="grid__col">
-                    <div className="registerInput">
-                      <input type="text" placeholder="Tên" ref={lastName} />
-                    </div>
-                  </div>
-                </div>
-                <div className=" grid__col-12-12">
-                  <div className="grid__col">
-                    <div className="registerInput">
-                      <input type="text" placeholder="Số di động hoặc email" ref={email} />
-                    </div>
-                  </div>
-                </div>
-                <div className=" grid__col-12-12">
-                  <div className="grid__col">
-                    <div className="registerInput">
-                      <input type="text" placeholder="Mật khẩu" ref={password} />
-                    </div>
-                  </div>
-                </div>
-                <div className="selectWrap">
-                  <div className="selectWrapDesc">Ngày sinh</div>
-                  <div className=" grid__col-4-12">
-                    <div className="grid__col">
-                      <div className="registerInput">
-                        <select ref={dayOfBirth}>
-                          {Array.from(new Array(31)).map((x, index) => (
-                            <option key={index} value={index + 1}>
-                              {index + 1}
-                            </option>
-                          ))}
-                        </select>
-                      </div>
-                    </div>
-                  </div>
-                  <div className=" grid__col-4-12">
-                    <div className="grid__col">
-                      <div className="registerInput">
-                        <select ref={monthOfBirth}>
-                          {Array.from(new Array(12)).map((x, index) => (
-                            <option key={index} value={index + 1}>{`Tháng ${index + 1}`}</option>
-                          ))}
-                        </select>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className=" grid__col-4-12">
-                    <div className="grid__col">
-                      <div className="registerInput">
-                        <select name="cars" id="cars" ref={yearOfBirth}>
-                          {Array.from(new Array(120)).map((x, index) => (
-                            <option key={index} value={yyyy - index}>
-                              {yyyy - index}
-                            </option>
-                          ))}
-                        </select>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="selectWrap">
-                  <div className=" grid__col-4-12">
-                    <div className="grid__col">
-                      <div className="registerInput">
-                        <label htmlFor="male">Nữ</label>
-                        <input type="radio" id="male" name="sex" ref={sex} value="male" />
-                      </div>
-                    </div>
-                  </div>
-                  <div className=" grid__col-4-12">
-                    <div className="grid__col">
-                      <div className="registerInput">
-                        <label htmlFor="female">Nam</label>
-                        <input type="radio" id="female" name="sex" ref={sex} value="female" />
-                      </div>
-                    </div>
-                  </div>
-                  <div className=" grid__col-4-12">
-                    <div className="grid__col">
-                      <div className="registerInput">
-                        <label htmlFor="orther">Khác</label>
-                        <input type="radio" id="orther" name="sex" ref={sex} value="orther" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <button type="submit" className="registerBtn">
-                Đăng ký
-              </button>
-            </form>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
