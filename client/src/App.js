b7268d8aff57679de18658b8cefb5644e637b534
buivantan01 <buivantan1234566@gmail.com>
2021-11-29 09:16:05 +0700

    Fixed: Updated section login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,67 @@
-import { useContext, useEffect, useRef } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { io } from 'socket.io-client';
-import './App.css';
-import Chat from './components/Chat';
-import { AuthContext } from './context/AuthProvider';
-import Home from './pages/home';
+// import { useContext, useEffect, useRef } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { io } from 'socket.io-client';
+// import './App.css';
+// import Chat from './components/Chat';
+// import { AuthContext } from './context/AuthProvider';
+// import Home from './pages/home';
+// import Login from './pages/login';
+// import Register from './pages/register';
+// import Profile from './pages/profile';
+// import Story from './pages/story';
+
+// function App() {
+//   const { user: currentUser } = useContext(AuthContext);
+
+//   // Ifsocket
+//   const socket = useRef();
+//   useEffect(() => {
+//     socket.current = io('ws://localhost:8900');
+//   }, []);
+
+//   return (
+//     <Router>
+//       {currentUser ? <Home /> : <Register />}
+//       <Switch>
+//         <Route path="/" exact>
+//           <Route path="/register"></Route>
+//           <Route path="/profile/:username"></Route>
+//           <Route path="/stories">{currentUser ? <Story /> : <Login />}</Route>
+//         </Route>
+//         <Profile />
+//       </Switch>
+
+//       <Chat />
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+import Home from './pages/home/';
 import Login from './pages/login';
-import Register from './pages/register';
 import Profile from './pages/profile';
-import Story from './pages/story';
+import Register from './pages/register';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
+import Messenger from './pages/messenger/Messenger';
 
 function App() {
-  const { user: currentUser } = useContext(AuthContext);
-
-  // Ifsocket
-  const socket = useRef();
-  useEffect(() => {
-    socket.current = io('ws://localhost:8900');
-  }, []);
-
+  const { user } = useContext(AuthContext);
   return (
     <Router>
-      {currentUser ? <Home /> : <Register />}
       <Switch>
-        <Route path="/" exact>
-          <Route path="/register"></Route>
-          <Route path="/profile/:username"></Route>
-          <Route path="/stories">{currentUser ? <Story /> : <Login />}</Route>
+        <Route exact path="/">
+          {user ? <Home /> : <Register />}
         </Route>
-        <Profile />
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">{user ? <Redirect to="/" /> : <Register />}</Route>
+        <Route path="/messenger">{!user ? <Redirect to="/" /> : <Messenger />}</Route>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
       </Switch>
-
-      <Chat />
     </Router>
   );
 }
--- client/src/pages/login/index.jsx
+++ client/src/pages/login/index.jsx
@@ -1,79 +1,129 @@
-import React, { useContext, useRef, useState } from 'react';
-import './Login.css';
-import { loginCall, registerCall } from '../../context/useAuth';
-import { AuthContext } from '../../context/AuthProvider';
+// import React, { useContext, useRef, useState } from 'react';
+// import './Login.css';
+// import { loginCall, registerCall } from '../../context/useAuth';
+// import { AuthContext } from '../../context/AuthProvider';
 
-Login.propTypes = {};
+// Login.propTypes = {};
 
-function Login(props) {
-  const [openRegister, setOpenRegister] = useState(false);
+// function Login(props) {
+//   const [openRegister, setOpenRegister] = useState(false);
 
-  const yyyy = new Date().getFullYear();
+//   const yyyy = new Date().getFullYear();
 
+//   const email = useRef();
+//   const password = useRef();
+//   const firstName = useRef();
+//   const lastName = useRef();
+//   const dayOfBirth = useRef();
+//   const monthOfBirth = useRef();
+//   const yearOfBirth = useRef();
+//   const sex = useRef();
+
+//   const { user, isFetching, dispatch } = useContext(AuthContext);
+
+//   const handleLogin = (e) => {
+//     e.preventDefault();
+//     loginCall({ email: email.current.value, password: password.current.value }, dispatch);
+//   };
+
+//     registerCall(newUser, dispatch);
+//     window.location.reload();
+//   };
+
+//   console.log(user);
+//   return (
+//     <div className="login">
+//       <div className="loginWrap">
+//         <div className="loginLeft">
+//           <img src="./assets/loginLogo.svg" alt="" className="loginLeftLogo" />
+//           <h3 className="loginLeftDesc">
+//             Facebook giúp bạn kết nối và chia sẻ với mọi người trong cuộc sống của bạn.
+//           </h3>
+//         </div>
+//         <div className="loginRight">
+//           <form className="loginRightForm" onSubmit={handleLogin}>
+//             <div className="loginRightFormInput">
+//               <input type="text" placeholder="Email" ref={email} />
+//             </div>
+//             <div className="loginRightFormInput">
+//               <input type="password" placeholder="Mật khẩu" ref={password} />
+//             </div>
+
+//             {isFetching ? (
+//               <button type="submit" className="loginRightFormBtnSubmit disabled" disabled>
+//                 <div className="lds-ring">
+//                   <div></div>
+//                   <div></div>
+//                   <div></div>
+//                   <div></div>
+//                 </div>
+//               </button>
+//             ) : (
+//               <button type="submit" className="loginRightFormBtnSubmit">
+//                 Đăng nhập
+//               </button>
+//             )}
+
+//             <div className="loginRightFormForgotPassword">Quên mật khẩu?</div>
+//             <hr className="loginRightFormHr" />
+//             <div className="loginRightFormNav" onClick={() => setOpenRegister(true)}>
+//               Tạo tài khoản mới
+//             </div>
+//           </form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
+import { useContext, useRef } from 'react';
+import './login.css';
+import { loginCall } from '../../apiCalls';
+import { AuthContext } from '../../context/AuthContext';
+import { CircularProgress } from '@material-ui/core';
+
+export default function Login() {
   const email = useRef();
   const password = useRef();
-  const firstName = useRef();
-  const lastName = useRef();
-  const dayOfBirth = useRef();
-  const monthOfBirth = useRef();
-  const yearOfBirth = useRef();
-  const sex = useRef();
+  const { isFetching, dispatch } = useContext(AuthContext);
 
-  const { user, isFetching, dispatch } = useContext(AuthContext);
-
-  const handleLogin = (e) => {
+  const handleClick = (e) => {
     e.preventDefault();
     loginCall({ email: email.current.value, password: password.current.value }, dispatch);
   };
 
-    registerCall(newUser, dispatch);
-    window.location.reload();
-  };
-
-  console.log(user);
   return (
     <div className="login">
-      <div className="loginWrap">
+      <div className="loginWrapper">
         <div className="loginLeft">
-          <img src="./assets/loginLogo.svg" alt="" className="loginLeftLogo" />
-          <h3 className="loginLeftDesc">
-            Facebook giúp bạn kết nối và chia sẻ với mọi người trong cuộc sống của bạn.
-          </h3>
+          <h3 className="loginLogo">Lamasocial</h3>
+          <span className="loginDesc">
+            Connect with friends and the world around you on Lamasocial.
+          </span>
         </div>
         <div className="loginRight">
-          <form className="loginRightForm" onSubmit={handleLogin}>
-            <div className="loginRightFormInput">
-              <input type="text" placeholder="Email" ref={email} />
-            </div>
-            <div className="loginRightFormInput">
-              <input type="password" placeholder="Mật khẩu" ref={password} />
-            </div>
-
-            {isFetching ? (
-              <button type="submit" className="loginRightFormBtnSubmit disabled" disabled>
-                <div className="lds-ring">
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                </div>
-              </button>
-            ) : (
-              <button type="submit" className="loginRightFormBtnSubmit">
-                Đăng nhập
-              </button>
-            )}
-
-            <div className="loginRightFormForgotPassword">Quên mật khẩu?</div>
-            <hr className="loginRightFormHr" />
-            <div className="loginRightFormNav" onClick={() => setOpenRegister(true)}>
-              Tạo tài khoản mới
-            </div>
+          <form className="loginBox" onSubmit={handleClick}>
+            <input placeholder="Email" type="email" required className="loginInput" ref={email} />
+            <input
+              placeholder="Password"
+              type="password"
+              required
+              minLength="6"
+              className="loginInput"
+              ref={password}
+            />
+            <button className="loginButton" type="submit" disabled={isFetching}>
+              {isFetching ? <CircularProgress color="white" size="20px" /> : 'Log In'}
+            </button>
+            <span className="loginForgot">Forgot Password?</span>
+            <button className="loginRegisterButton">
+              {isFetching ? <CircularProgress color="white" size="20px" /> : 'Create a New Account'}
+            </button>
           </form>
         </div>
       </div>
     </div>
   );
 }
-
-export default Login;
