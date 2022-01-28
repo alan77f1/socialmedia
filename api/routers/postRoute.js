d6128fe2e39760751df8bfd10b1ca06d905ee7d6
buivantan01 <buivantan1234566@gmail.com>
2022-01-28 07:44:54 +0700

    Refactor: Build back postController

--- api/routers/postRoute.js
+++ api/routers/postRoute.js
@@ -1,105 +1,22 @@
 const express = require('express');
 const User = require('../models/User');
-const messageController = require('../controllers/messageController');
+const postController = require('../controllers/postController');
 const Post = require('./../models/Post');
 
 const router = express.Router();
 // create: [POST] api/posts
-router.post('/', async function (req, res) {
-  const newPost = new Post(req.body);
-  try {
-    const savedPost = await newPost.save();
-    res.status(200).json(savedPost);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.post('/', postController.post);
 
 // get my post and friends'post: [GET] api/posts
-router.get('/:userId', async function (req, res) {
-  try {
-    const currentUser = await User.findOne({ _id: req.params.userId });
-
-    const myPosts = await Post.find({ userId: req.params.userId }); // get my post
-
-    const followingPost = await Promise.all(
-      currentUser.followingIds.map((id) => {
-        return Post.find({ userId: id });
-      })
-    );
-
-    res.json(myPosts.concat(...followingPost));
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.get('/:userId', postController.getFriendPosts);
 
 // get my post [GET] api/posts/myposts
-router.get('/:userId/me', async function (req, res) {
-  try {
-    let myPosts;
-    if (req.params.userId) {
-      myPosts = await Post.find({ userId: req.params.userId }); // get my post
-    } else {
-      const currentUser = await User.findOne({ _id: req.params.username });
-      myPosts = await Post.find({ userId: currentUser._id }); // get my post
-    }
-    res.json(myPosts);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.get('/:userId/me', postController.getMyPosts);
 
 // // changelikes post
-router.put('/:id/changelikes', async function (req, res) {
-  try {
-    const post = await Post.findOne({ _id: req.params.id });
-    await post.updateOne({
-      $pull: { likes: { userId: req.body.userId } },
-    });
-    // like post
-    await post.updateOne({
-      $push: {
-        likes: {
-          type: req.body.type,
-          userId: req.body.userId,
-          text: req.body.text,
-          styleColor: req.body.styleColor,
-        },
-      },
-    });
-    res.status(200).json('The post has been ' + req.body.type);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.put('/:id/changelikes', postController.putChangelikes);
 
 // like post
-router.put('/:id/likes', async function (req, res) {
-  try {
-    const post = await Post.findOne({ _id: req.params.id });
-    if (!post.likes.some((like) => like['userId'] === req.body.userId)) {
-      // like post
-      await post.updateOne({
-        $push: {
-          likes: {
-            type: req.body.type,
-            userId: req.body.userId,
-            text: req.body.text,
-            styleColor: req.body.styleColor,
-          },
-        },
-      });
-      res.status(200).json('The post has been ' + req.body.type);
-    } else {
-      await post.updateOne({
-        $pull: { likes: { userId: req.body.userId } },
-      });
-      res.status(200).json('The post has dislike ');
-    }
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.put('/:id/likes', postController.putLikePosts);
 
 module.exports = router;
