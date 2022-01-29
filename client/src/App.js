6f6f7f9d6617eb4a962496059b84ef3fe5b3f496
buivantan01 <buivantan1234566@gmail.com>
2022-01-29 08:55:09 +0700

    Refactor: fixed something app

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,36 @@
-// import { useContext, useEffect, useRef } from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import { io } from 'socket.io-client';
-// import './App.css';
-// import Chat from './components/Chat';
-// import { AuthContext } from './context/AuthProvider';
-// import Home from './pages/home';
-// import Login from './pages/login';
-// import Register from './pages/register';
-// import Profile from './pages/profile';
-// import Story from './pages/story';
-
-// function App() {
-//   const { user: currentUser } = useContext(AuthContext);
-
-//   // Ifsocket
-//   const socket = useRef();
-//   useEffect(() => {
-//     socket.current = io('ws://localhost:8900');
-//   }, []);
-
-//   return (
-//     <Router>
-//       {currentUser ? <Home /> : <Register />}
-//       <Switch>
-//         <Route path="/" exact>
-//           <Route path="/register"></Route>
-//           <Route path="/profile/:username"></Route>
-//           <Route path="/stories">{currentUser ? <Story /> : <Login />}</Route>
-//         </Route>
-//         <Profile />
-//       </Switch>
-
-//       <Chat />
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-import Home from './pages/home/';
+import { useContext, useEffect, useRef } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { io } from 'socket.io-client';
+import './App.css';
+import Chat from './components/Chat';
+import Header from './components/Header';
+import { AuthContext } from './context/AuthProvider';
+import Home from './pages/home';
 import Login from './pages/login';
 import Profile from './pages/profile';
-import Register from './pages/register';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import { useContext } from 'react';
-import { AuthContext } from './context/AuthContext';
-import Messenger from './pages/messenger/Messenger';
+import Story from './pages/story';
 
 function App() {
-  const { user } = useContext(AuthContext);
+  const { user: currentUser } = useContext(AuthContext);
+
+  // socket
+  // const socket = useRef();
+  // useEffect(() => {
+  //     socket.current = io('ws://localhost:8900');
+  // }, []);
+
   return (
     <Router>
+      {currentUser && <Header />}
       <Switch>
-        <Route exact path="/">
-          {user ? <Home /> : <Register />}
-        </Route>
-        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
-        <Route path="/register">{user ? <Redirect to="/" /> : <Register />}</Route>
-        <Route path="/messenger">{!user ? <Redirect to="/" /> : <Messenger />}</Route>
-        <Route path="/profile/:username">
-          <Profile />
+        <Route path="/profile">{currentUser ? <Profile /> : <Login />}</Route>
+        <Route path="/stories">{currentUser ? <Story /> : <Login />}</Route>
+        <Route path="/" exact>
+          {currentUser ? <Home /> : <Login />}
         </Route>
       </Switch>
+
+      <Chat />
     </Router>
   );
 }
