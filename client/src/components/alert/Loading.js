bd3ac14c50d4b8a118bb31348a135371ce449db1
buivantan01 <buivantan1234566@gmail.com>
2022-04-18 21:49:03 +0700

    Make it better

--- client/src/components/alert/Loading.js
+++ client/src/components/alert/Loading.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <div
-      className="container position-fixed w-100 h-100 text-center loading"
-      style={{ background: '#0008', color: 'white', top: 0, left: 0, right: 0, zIndex: 50 }}
-    >
+    <div class="container">
       <svg>
         <circle cx="150" cy="150" r="140"></circle>
         <circle cx="150" cy="150" r="100"></circle>
       </svg>
-      <p>loading...</p>
+
+      <text fill="#fff" x="5" y="47">
+        loading...
+      </text>
     </div>
   );
 };
