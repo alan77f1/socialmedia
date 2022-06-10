cfd64653b0168222bc9392d46925f11ecff8d58a
buivantan01 <buivantan1234566@gmail.com>
2022-06-10 21:57:54 +0700

    Refactor

--- client/src/utils/imageUpload.js
+++ client/src/utils/imageUpload.js
@@ -1,13 +1,12 @@
 export const checkImage = (file) => {
-  let err = "";
-  if (!file) return (err = "Không tìm thấy tập tin.");
+  let err = '';
+  if (!file) return (err = 'Không tìm thấy tập tin.');
 
   if (file.size > 1024 * 1024 * 2)
     // 1mb
-    err = "Dung lượng tối đa 2mb.";
+    err = 'Dung lượng tối đa 2mb.';
 
-  if (file.type !== "image/jpeg" && file.type !== "image/png")
-    err = "Định dạng không được hỗ trợ.";
+  if (file.type !== 'image/jpeg' && file.type !== 'image/png') err = 'Định dạng không được hỗ trợ.';
 
   return err;
 };
@@ -18,21 +17,18 @@ export const imageUpload = async (images) => {
     const formData = new FormData();
 
     if (item.camera) {
-      formData.append("file", item.camera);
+      formData.append('file', item.camera);
     } else {
-      formData.append("file", item);
+      formData.append('file', item);
     }
 
-    formData.append("upload_preset", "efxjficn");
-    formData.append("cloud_name", "devat-channel");
+    formData.append('upload_preset', 'j9oyl7cg');
+    formData.append('cloud_name', 'buivantan');
 
-    const res = await fetch(
-      "https://api.cloudinary.com/v1_1/devat-channel/upload",
-      {
-        method: "POST",
-        body: formData,
-      }
-    );
+    const res = await fetch('https://api.cloudinary.com/v1_1/buivantan/upload', {
+      method: 'POST',
+      body: formData,
+    });
 
     const data = await res.json();
     imgArr.push({ public_id: data.public_id, url: data.secure_url });
